f1d16cbcfaa95ed7faa2222f3cd958d01ec0336a
Hack Reactor Students <github@hackreactor.com>
2013-12-18 18:56:51 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var port        = process.env.PORT || 3000;
+var twilioSid   = process.env.TWILIO_SID;
+var twilioAuth  = process.env.TWILIO_TOKEN;
+
+var app = express();
+app.use(express.bodyParser());
+
+app.post('*', function(request, response){
+  console.log(request.body);
+  console.log(request.params);
+});
+
+app.listen(port);
